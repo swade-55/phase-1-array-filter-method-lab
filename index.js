79261a966bbe7ef49b9facdabaf1fe6b6df78e8e
SamuelWade <samuelrcwade@gmail.com>
2023-03-02 10:31:50 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(driverArray,driverString) {
+    return driverArray.filter(function(driver){return driver.toLowerCase() === driverString.toLowerCase()});
+}
+
+function fuzzyMatch(driverArray,driverString) {
+    return driverArray.filter(function(driver) {
+        if (driver.slice(0,2) === driverString.slice(0,2)) {
+            return driver;
+        } else {return null;}
+    });
+
+}
+
+function matchName(driverArray,driverString) {
+    console.log(driverArray);
+    return driverArray.filter(function(driver) {
+        return driver.name === driverString;
+    })
+    
+}
+
+
